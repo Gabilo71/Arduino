8930a01ca63c2d5c6b07d32774b42688534ded51
SBartolomeo <isanbar@upv.edu.es>
2025-11-14 13:46:20 +0100

    Refactor pin definitions and update logic
    Refactor pin definitions and update pinMode calls for better readability. Adjust state reading and motor control logic.

--- Codigo.ino
+++ Codigo.ino
@@ -1,41 +1,62 @@
-int alimentador_delante=2, alimentador_atras=3, marcha=4, entrada=5, salida=6, motor_delante=11, motor_detras=12, electroiman=13;
+
+//Definimos los pines de los elementos a utilizar de forma que si luego se conecta a otro pin solo haría falta realizar el cambio en el apartado siguiente.
+//Definimos los etados de estos que tomaran valores 0 o 1.
+
+int pin_alimentador_delante=2, pin_alimentador_atras=3, pin_marcha=4, pin_pieza_entrada=5, pin_pieza_salida=6, pin_motor_delante=11, pin_motor_detras=12, pin_electroiman=13;
+int estado_alimentador_delante, estado_alimentador_atras, estado_marcha,estado_pieza_entrada, estado_pieza_salida, estado_motor_delante, esatdo_motor_detras, estado_electroiman;
+int electroiman_con_pieza;
 bool estado;
 void setup()
 {
   //Entradas del sistema
-  pinMode(alimentador_delante,INPUT);
-  pinMode(alimentador_atras,INPUT);
-  pinMode(marcha,INPUT);
-  pinMode(entrada,INPUT);
-  pinMode(salida,INPUT);
+  pinMode(pin_alimentador_delante,INPUT);
+  pinMode(pin_alimentador_atras,INPUT);
+  pinMode(pin_pieza_marcha,INPUT);
+  pinMode(pin_pieza_entrada,INPUT);
+  pinMode(pin_salida,INPUT);
 
   //Salidas del sistema
-  pinMode(motor_delante,OUTPUT);
-  pinMode(motor_detras,OUTPUT);
-  pinMode(electroiman,OUTPUT);
+  pinMode(pin_motor_delante,OUTPUT);
+  pinMode(pin_motor_detras,OUTPUT);
+  pinMode(pin_electroiman,OUTPUT);
 }
 
 void loop()
 {
-  ali_delante=digitalRead(2); // Alimentador en posición delantera
-  ali_atras=digitalRead(3); // Alimentador en posición trasera
-  marcha=digitalRead(4); // Marcha del proceso
-  entrada=digitalRead(5); // Piezas en la entrada
-  salida=digitalRead(6); // Piezas en la salida
-  if (entrada==1 && marcha==1 && ali_delante==0){
-    digitalWrite(12,0);
-    digitalWrite(13,0);
-    digitalWrite(11,1);
-    while (ali_delante==0);
-    digitalWrite(11,0);
+  //Lecturas de los estados de los botones (Valores toman 0 o 1)
+  estado_alimentador_delante=digitalRead(2); 
+  estado_alimentador_atras=digitalRead(3); 
+  estado_marcha=digitalRead(4); 
+  estado_pieza_entrada=digitalRead(5);
+  estado_pieza_salida=digitalRead(6);
+
+  //Condición para que el motor viaje hacia delante. Interreptures encendidos: entrada, marcha, 
+  
+  if (estado_marcha==1 && estado_pieza_entrada==1 && estado_alimentador_delante==0){
+    //Suponemos las conciciones iniciales
+    digitalWrite(pin_motor_detras,0);
+    digitalWrite(pin_electroiman,0);
+
+    //enecendemos el motor
+    digitalWrite(pin_motor_delante,1);
+    while (estado_alimentador_delante==0){
+    }
+    digitalWrite(pin_motor_delante,0);
+    digitalWrite(pin_electroiman,1);
+    electroiman_con_pieza=1;
   }
-    else if (entrada==1 && salida==0 && ali_delante==1) {  
-    digitalWrite(13,1);
-    digitalWrite(11,0);
+    else if (estado_pieza_salida==0 && electroiman_con_pieza==1 && estado_alimentador_atras==0) {  
+      //condiciones iniciales
+    digitalWrite(pin_electroiman,1);
+    digitalWrite(pin_motor_delante,0);
     delay(1000);
-    digitalWrite(12,1);
-    while (ali_atras==0);
-    digitalWrite(12,0);
+
+      
+    digitalWrite(pin_motor_detras,1);
+    while (estado_alimentador_atras==0){
+    }
+    digitalWrite(pin_motor_detras,0);
+    digitalWrite(pin_electroiman,0):
     }
   }
 
