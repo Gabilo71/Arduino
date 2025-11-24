ea0745af2eccc359aacd72bb212c17302e388c35
SBartolomeo <isanbar@upv.edu.es>
2025-11-24 17:24:36 +0100

    Fix constructor parameter for Motor class

--- Motor.cpp
+++ Motor.cpp
@@ -1,7 +1,7 @@
 
 #include<Motor.h>
 
-Motor::Motor(pin_alimentador_delante) : _pin (pin_alimentador_delante);
+Motor::Motor(pin_motor) : _pin (pin_motor);
 
 void Motor::begin(){
   pinMode(_pin, OUTPUT);
