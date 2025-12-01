802ecc2d479d08fff4d761419bd072ad256070b0
SBartolomeo <isanbar@upv.edu.es>
2025-12-01 16:36:59 +0100

    Fix pin assignment and clean up comments

--- ARDUINO_FUN.ino
+++ ARDUINO_FUN.ino
@@ -11,7 +11,7 @@ DESARROLADORES: Gabriel Lopez & Ismael San Bartolomé
 //Definimos los etados de estos que tomaran valores 0 o 1.
 //#include <arduino.h>
 
-int pin_alimentador_delante=2, pin_alimentador_atras=3, pin_marcha=4, pin_pieza_entrada=5, pin_pieza_salida=6, pin_motor_delante=11, pin_motor_detras=12, pin_electroiman=13;
+int pin_alimentador_delante=2, pin_alimentador_atras=3, pin_marcha=4, pin_pieza_entrada=5, pin_pieza_salida=6, pin_motor_delante=11, pin_motor_detras=10, pin_electroiman=13;
 bool estado_alimentador_delante, estado_alimentador_atras, estado_marcha,estado_pieza_entrada, estado_pieza_salida, estado_motor_delante, esatdo_motor_detras, estado_electroiman;
 int electroiman_con_pieza;
 bool estado;
@@ -125,3 +125,4 @@ void loop()
   }
 */
   }
+
