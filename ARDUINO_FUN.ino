62dc3a7a1515894fa287c792944b7f68039568ed
SBartolomeo <isanbar@upv.edu.es>
2025-12-01 16:33:31 +0100

    Improve motor control and state management
    Refactor motor control logic and state readings.

--- ARDUINO_FUN.ino
+++ ARDUINO_FUN.ino
@@ -9,7 +9,7 @@ DESARROLADORES: Gabriel Lopez & Ismael San Bartolomé
 
 //Definimos los pines de los elementos a utilizar de forma que si luego se conecta a otro pin solo haría falta realizar el cambio en el apartado siguiente.
 //Definimos los etados de estos que tomaran valores 0 o 1.
-#include <arduino.h>
+//#include <arduino.h>
 
 int pin_alimentador_delante=2, pin_alimentador_atras=3, pin_marcha=4, pin_pieza_entrada=5, pin_pieza_salida=6, pin_motor_delante=11, pin_motor_detras=12, pin_electroiman=13;
 bool estado_alimentador_delante, estado_alimentador_atras, estado_marcha,estado_pieza_entrada, estado_pieza_salida, estado_motor_delante, esatdo_motor_detras, estado_electroiman;
@@ -38,19 +38,21 @@ void setup()
 }
 
 int posicion_inicial_trabajo(int pin_alimentador_atras){
-    estado_alimentador_atras=digitalRead(pin_alimentador_atras);
+    lecturas_estados_pines();
     if (estado_alimentador_atras==0){
         Serial.println("Arranque seguro: Desactivado");
         Serial.println("Preparando motor");
-    while(estado_alimentador_atras==0){
+    while(estado_alimentador_delante==0){
+      lecturas_estados_pines();
         digitalWrite(pin_motor_delante,1);
         electroiman(0);
-        estado_alimentador_atras=digitalRead(pin_alimentador_atras);
+        
     }
     digitalWrite(pin_motor_delante,0);
-    electroiman(1);
-    delay(500);
+    /*electroiman(1); //Esto de aqui hay que verlo
+    delay(5000);
     electroiman(0);
+    */
     return 1;
     }
     else{
@@ -70,12 +72,13 @@ void lecturas_estados_pines(){
 void movimiento_adelante_motor(){
     Serial.println("Descargando pieza");
     electroiman(0);
-    delay(500);
+    delay(5000);
     Serial.println("Apilador descargado");
 
     Serial.println("Volviendo a posicion de carga");
 
     while (estado_alimentador_delante==0){
+      lecturas_estados_pines();
       digitalWrite(pin_motor_delante,1);
     }
     digitalWrite(pin_motor_delante,0);
@@ -85,12 +88,13 @@ void movimiento_atras_motor(){
     
     Serial.println("Cargando pieza");
     electroiman(1);
-    delay(500);
+    delay(5000);
     Serial.println("Apilador cargado");
 
     Serial.println("Transportando");
 
     while (estado_alimentador_atras==0){
+      lecturas_estados_pines();
       digitalWrite(pin_motor_detras,1);
     }
     digitalWrite(pin_motor_detras,0);
@@ -104,9 +108,11 @@ void electroiman(int estado){
 void loop()
 {
   //Lecturas de los estados de los botones (Valores toman 0 o 1)
-  void lecturas_pines();
+  lecturas_estados_pines();
   
-  void movimiento_adelante_motor();
+  movimiento_atras_motor();
+
+  movimiento_adelante_motor();
 
   /*if(estado_alimentador_atras==true&&estado_marcha==true&&estado_electroiman==true){
     movimiento_adelante_motor();
