b9d2d11547b6c5aeb60f434526139b1d3a3b2de7
SBartolomeo <isanbar@upv.edu.es>
2025-12-01 15:39:10 +0100

    Refactor ARDUINO_FUN.ino with improved logic

--- ARDUINO_FUN.ino
+++ ARDUINO_FUN.ino
@@ -11,23 +11,21 @@ DESARROLADORES: Gabriel Lopez & Ismael San Bartolomé
 //Definimos los etados de estos que tomaran valores 0 o 1.
 #include <arduino.h>
 
-void loop()
-{
-    
-}
-
 int pin_alimentador_delante=2, pin_alimentador_atras=3, pin_marcha=4, pin_pieza_entrada=5, pin_pieza_salida=6, pin_motor_delante=11, pin_motor_detras=12, pin_electroiman=13;
 bool estado_alimentador_delante, estado_alimentador_atras, estado_marcha,estado_pieza_entrada, estado_pieza_salida, estado_motor_delante, esatdo_motor_detras, estado_electroiman;
 int electroiman_con_pieza;
 bool estado;
+
 void setup()
 {
+  Serial.begin(9600);
+
   //Entradas del sistema
   pinMode(pin_alimentador_delante,INPUT);
   pinMode(pin_alimentador_atras,INPUT);
-  pinMode(pin_pieza_marcha,INPUT);
+  pinMode(pin_marcha,INPUT);
   pinMode(pin_pieza_entrada,INPUT);
-  pinMode(pin_salida,INPUT);
+  pinMode(pin_pieza_salida,INPUT);
 
   //Salidas del sistema
   pinMode(pin_motor_delante,OUTPUT);
@@ -35,7 +33,7 @@ void setup()
   pinMode(pin_electroiman,OUTPUT);
   
   if (posicion_inicial_trabajo(pin_alimentador_atras)==1){
-    Serial.println("Arranque seguro: Activado:");
+    Serial.println("Arranque seguro: Activado");
   }
 }
 
@@ -45,23 +43,23 @@ int posicion_inicial_trabajo(int pin_alimentador_atras){
         Serial.println("Arranque seguro: Desactivado");
         Serial.println("Preparando motor");
     while(estado_alimentador_atras==0){
-        digitalWrite(11,1);
-        digitalWrite(13,0);
+        digitalWrite(pin_motor_delante,1);
+        electroiman(0);
         estado_alimentador_atras=digitalRead(pin_alimentador_atras);
     }
-    digitalWrite(11,0);
-    digitalWrite(13,1);
+    digitalWrite(pin_motor_delante,0);
+    electroiman(1);
     delay(500);
-    digitalWrite(13,0);
+    electroiman(0);
     return 1;
     }
     else{
         return 1;
     }
-;
+
 }
 
-void lecturas_estados_pines(int pin_alimentador_delante,int pin_alimentador_atras,int pin_marcha,int pin_pieza_entrada,int pin_pieza_salida){
+void lecturas_estados_pines(){
   estado_alimentador_delante=digitalRead(pin_alimentador_delante); 
   estado_alimentador_atras=digitalRead(pin_alimentador_atras); 
   estado_marcha=digitalRead(pin_marcha); 
@@ -70,23 +68,35 @@ void lecturas_estados_pines(int pin_alimentador_delante,int pin_alimentador_atra
 }
 
 void movimiento_adelante_motor(){
-   //Suponemos las conciciones iniciales
-    digitalWrite(pin_motor_delante,1);
+    Serial.println("Descargando pieza");
+    electroiman(0);
+    delay(500);
+    Serial.println("Apilador descargado");
+
+    Serial.println("Volviendo a posicion de carga");
+
     while (estado_alimentador_delante==0){
-      digitalRead(pin_alimentador_delante);
+      digitalWrite(pin_motor_delante,1);
     }
     digitalWrite(pin_motor_delante,0);
-    
+    Serial.println("Preparado para carga");
 }
 void movimiento_atras_motor(){
-   //Suponemos las conciciones iniciales
-    digitalWrite(pin_motor_atras,1);
-    while (estado_alimentador_atras==0){
-      digitalRead(pin_alimentador_atras);
-    }
-    digitalWrite(pin_motor_atras,0);
     
+    Serial.println("Cargando pieza");
+    electroiman(1);
+    delay(500);
+    Serial.println("Apilador cargado");
+
+    Serial.println("Transportando");
+
+    while (estado_alimentador_atras==0){
+      digitalWrite(pin_motor_detras,1);
+    }
+    digitalWrite(pin_motor_detras,0);
+    Serial.println("Preparado para descarga");
 }
+
 void electroiman(int estado){
   digitalWrite(pin_electroiman,estado);
 }
@@ -94,18 +104,18 @@ void electroiman(int estado){
 void loop()
 {
   //Lecturas de los estados de los botones (Valores toman 0 o 1)
-  void lecturas_pines(pin_alimentador_delante,pin_alimentador_atras,pin_marcha,pin_pieza_entrada,pin_pieza_salida);
+  void lecturas_pines();
   
-  electroiman(0);
+  void movimiento_adelante_motor();
 
-  if(estado_alimentador_atras==true&&estado_marcha==true&&estado_electroiman==true){
+  /*if(estado_alimentador_atras==true&&estado_marcha==true&&estado_electroiman==true){
     movimiento_adelante_motor();
   }
 
-  electroiman(1);
+
   
   if(estado_alimentador_delante==true&&estado_marcha==true&&estado_electroiman==true){
     movimiento_atras_motor();
   }
-
+*/
   }
