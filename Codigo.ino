273aedb53a39636ab6f84af7352b3e2382bd8d39
SBartolomeo <isanbar@upv.edu.es>
2025-12-04 15:04:44 +0100

    Update pin definitions and motor control functions
    Refactor motor control logic and pin definitions.

--- Codigo.ino
+++ Codigo.ino
@@ -9,75 +9,116 @@ DESARROLADORES: Gabriel Lopez & Ismael San Bartolomé
 
 //Definimos los pines de los elementos a utilizar de forma que si luego se conecta a otro pin solo haría falta realizar el cambio en el apartado siguiente.
 //Definimos los etados de estos que tomaran valores 0 o 1.
+//#include <arduino.h>
 
-int pin_alimentador_delante=2, pin_alimentador_atras=3, pin_marcha=4, pin_pieza_entrada=5, pin_pieza_salida=6, pin_motor_delante=11, pin_motor_detras=12, pin_electroiman=13;
-int estado_alimentador_delante, estado_alimentador_atras, estado_marcha,estado_pieza_entrada, estado_pieza_salida, estado_motor_delante, esatdo_motor_detras, estado_electroiman;
+int pin_alimentador_delante=2, pin_alimentador_atras=3, pin_marcha=4, pin_pieza_entrada=5, pin_pieza_salida=6, pin_motor_delante=11, pin_motor_detras=10, pin_electroiman=13;
+bool estado_alimentador_delante, estado_alimentador_atras, estado_marcha,estado_pieza_entrada, estado_pieza_salida, estado_motor_delante, esatdo_motor_detras, estado_electroiman;
 int electroiman_con_pieza;
 bool estado;
+
 void setup()
 {
+  Serial.begin(9600);
+
   //Entradas del sistema
   pinMode(pin_alimentador_delante,INPUT);
   pinMode(pin_alimentador_atras,INPUT);
-  pinMode(pin_pieza_marcha,INPUT);
+  pinMode(pin_marcha,INPUT);
   pinMode(pin_pieza_entrada,INPUT);
-  pinMode(pin_salida,INPUT);
+  pinMode(pin_pieza_salida,INPUT);
 
   //Salidas del sistema
   pinMode(pin_motor_delante,OUTPUT);
   pinMode(pin_motor_detras,OUTPUT);
   pinMode(pin_electroiman,OUTPUT);
-  //Posición inicial de trabajo
-  estado_alimentador_atras=digitalRead(pin_alimentador_atras);
-  if (estado_alimentador_atras==0){
-  while(estado_alimentador_atras==0){
-    digitalWrite(11,1);
-    digitalWrite(13,0);
-    estado_alimentador_atras=digitalRead(pin_alimentador_atras);
-    }
-    digitalWrite(11,0);
-    digitalWrite(13,1);
-    delay(500);
-    digitalWrite(13,0);
+  
+  if (posicion_inicial_trabajo(pin_alimentador_atras)==1){
+    Serial.println("Arranque seguro: Activado");
   }
 }
 
-void loop()
-{
-  //Lecturas de los estados de los botones (Valores toman 0 o 1)
+int posicion_inicial_trabajo(int pin_alimentador_atras){
+    lecturas_estados_pines();
+    if (estado_alimentador_atras==0){
+        Serial.println("Arranque seguro: Desactivado");
+        Serial.println("Preparando motor");
+    while(estado_alimentador_atras==0){
+      lecturas_estados_pines();
+        digitalWrite(pin_motor_detras,1);
+        electroiman(0);
+        
+    }
+    digitalWrite(pin_motor_detras,0);
+    /*electroiman(1); //Esto de aqui hay que verlo
+    delay(5000);
+    electroiman(0);
+    */
+    return 1;
+    }
+    else{
+        return 1;
+    }
+
+}
+
+void lecturas_estados_pines(){
   estado_alimentador_delante=digitalRead(pin_alimentador_delante); 
   estado_alimentador_atras=digitalRead(pin_alimentador_atras); 
   estado_marcha=digitalRead(pin_marcha); 
   estado_pieza_entrada=digitalRead(pin_pieza_entrada);
   estado_pieza_salida=digitalRead(pin_pieza_salida);
+  estado_electroiman=digitalRead(pin_electroiman);
+}
 
-  //Condición para que el motor viaje hacia delante. Interruptores encendidos: entrada, marcha, 
-  
-  if (estado_marcha==1 && estado_pieza_entrada==1 && estado_alimentador_delante==0){
-    //Suponemos las conciciones iniciales
-    digitalWrite(pin_motor_detras,0);
-    digitalWrite(pin_electroiman,0);
+void movimiento_adelante_motor(){
+    Serial.println("Descargando pieza");
+    electroiman(0);
+    delay(5000);
+    Serial.println("Apilador descargado");
 
-    //Enecendemos el motor
-    digitalWrite(pin_motor_delante,1);
+    Serial.println("Volviendo a posicion de carga");
+
     while (estado_alimentador_delante==0){
+      lecturas_estados_pines();
+      digitalWrite(pin_motor_delante,1);
     }
     digitalWrite(pin_motor_delante,0);
-    digitalWrite(pin_electroiman,1);
-    electroiman_con_pieza=1;
-  }
-    else if (estado_pieza_salida==0 && electroiman_con_pieza==1 && estado_alimentador_atras==0) {  
-      //Condiciones iniciales
-    digitalWrite(pin_electroiman,1);
-    digitalWrite(pin_motor_delante,0);
-      //Delay para segurasrse que el electroimán coge la pieza
-    delay(1000);
+    Serial.println("Preparado para carga");
+}
+void movimiento_atras_motor(){
+    
+    Serial.println("Cargando pieza");
+    electroiman(1);
+    delay(5000);
+    Serial.println("Apilador cargado");
 
-      
-    digitalWrite(pin_motor_detras,1);
+    Serial.println("Transportando");
+
     while (estado_alimentador_atras==0){
+      lecturas_estados_pines();
+      digitalWrite(pin_motor_detras,1);
     }
     digitalWrite(pin_motor_detras,0);
-    digitalWrite(pin_electroiman,0):
-    }
+    Serial.println("Preparado para descarga");
+}
+
+void electroiman(int estado){
+  digitalWrite(pin_electroiman,estado);
+}
+
+void loop()
+{
+  //Lecturas de los estados de los botones (Valores toman 0 o 1)
+  lecturas_estados_pines();
+  
+  if(estado_alimentador_atras==true&&estado_marcha==true&&estado_pieza_entrada==true){
+    movimiento_adelante_motor();
+  }
+
+
+  
+  if(estado_alimentador_delante==true&&estado_marcha==true&&estado_pieza_salida==false){
+    movimiento_atras_motor();
+  }
+
   }
